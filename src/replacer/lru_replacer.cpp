6cb53b480e18f521fe78e60413acf32d75059ed5
z2z63 <vituralfuture@gmail.com>
2024-05-27 16:35:40 +0800

    fix: add frame_id range check

--- src/replacer/lru_replacer.cpp
+++ src/replacer/lru_replacer.cpp
@@ -13,14 +13,14 @@ See the Mulan PSL v2 for more details. */
 
 LRUReplacer::LRUReplacer(size_t num_pages) { max_size_ = num_pages; }
 
-LRUReplacer::~LRUReplacer() = default;  
+LRUReplacer::~LRUReplacer() = default;
 
 /**
  * @description: 使用LRU策略删除一个victim frame，并返回该frame的id
  * @param {frame_id_t*} frame_id 被移除的frame的id，如果没有frame被移除返回nullptr
  * @return {bool} 如果成功淘汰了一个页面则返回true，否则返回false
  */
-bool LRUReplacer::victim(frame_id_t* frame_id) {
+bool LRUReplacer::victim(frame_id_t *frame_id) {
     // C++17 std::scoped_lock
     // 它能够避免死锁发生，其构造函数能够自动进行上锁操作，析构函数会对互斥量进行解锁操作，保证线程安全。
     std::scoped_lock lock{latch_};  //  如果编译报错可以替换成其他lock
@@ -49,11 +49,13 @@ void LRUReplacer::pin(frame_id_t frame_id) {
     // 固定指定id的frame
     // 在数据结构中移除该frame
     auto it = LRUhash_.find(frame_id);
-    if(it != LRUhash_.end()){
+    if (it != LRUhash_.end()) {
         LRUlist_.erase(it->second);
         LRUhash_.erase(LRUhash_.find(frame_id));
-    }else{
-        throw InternalError("LRUReplacer::pin try to pin a already pinned page");
+    }else if(frame_id < 0 || frame_id >= max_size_) {
+        char msg[80];
+        std::snprintf(msg, 80, "LRUReplacer::pin invalid frame_id: %d", frame_id);
+        throw InternalError(std::string(msg));
     }
 }
 
@@ -70,8 +72,13 @@ void LRUReplacer::unpin(frame_id_t frame_id) {
         auto it = LRUhash_[frame_id];
 //        it = LRUlist_.erase(it);
 //        LRUlist_.push_front(frame_id);    // ？命中LRU链表时不需要移动到链表头，否则无法通过测试
-    }else{
+    } else if (frame_id >= 0 && frame_id < max_size_) {
         LRUlist_.push_front(frame_id);
+    } else{
+//        处理非法情况
+        char msg[80];
+        std::snprintf(msg, 80, "LRUReplacer::unpin invalid frame_id: %d", frame_id);
+        throw InternalError(std::string(msg));
     }
     LRUhash_[frame_id] = LRUlist_.begin();      // 插入不会使迭代器失效
 }
