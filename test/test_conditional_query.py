7ca4dcbff4d4982f20fa78f7cb6309807f3aa97f
z2z63 <vituralfuture@gmail.com>
2024-05-30 00:40:17 +0800

    test: add delete test and conditional select test

--- test/test_conditional_query.py
+++ test/test_conditional_query.py
@@ -36,19 +36,21 @@ class TestConditionalQuery:
     def setup_method(cls):  # 每次测试都启动一个新的客户端
         cls.client = subprocess.Popen([cls.CLIENT], stdin=subprocess.PIPE, preexec_fn=os.setsid)
 
-
     @classmethod
     @pytest.mark.parametrize('_round', range(100))
     def test_insert(cls, _round):
         cls.TABLENAME = cls._TABLENAME + str(_round)
         column_types, column_names, mock = cls.create_and_insert()
-        for i in range(50):     # 随机更新部分记录
-            cls.random_update(column_types, column_names, mock)
+        for i in range(50):  # 考虑每次都删除记录的情况，要保证不把记录删完
+            if random.random() < 0.8:  # 随机更新部分记录
+                cls.random_update(column_types, column_names, mock)
+            else:  # 随机删除部分记录
+                cls.random_delete(column_types, column_names, mock)
+        # 扫全表，检查每条记录是否正确
         cls.client.stdin.write(f"select * from {cls.TABLENAME};\n".encode())
         cls.client.stdin.close()
         time.sleep(1)
-        with open(f"{cls.DB}/output.txt", "w") as f:    # 清空输出，避免上轮输出的结果影响这轮测试
-            f.close()
+        open(f"{cls.DB}/output.txt", "w").close()  # 清空输出，避免上轮输出的结果影响这轮测试
         with open(f"{cls.DB}/output.txt", "rt") as f:
             lines = f.readlines()
         for row_no, line in enumerate(lines[1:]):
@@ -63,6 +65,44 @@ class TestConditionalQuery:
                     assert lhs == rhs.strip("'")
                 else:
                     assert False, '不支持的类型'
+
+        # # 以下测试多次重复后会出错，原因未知，只能重复测试一两次
+        # # 检查条件查询的结果是否正确
+        # for i in range(int(len(mock) * 0.4)):  # 抽样40%的数据
+        #     # 随机选取一行
+        #     row_no = random.randint(0, len(mock) - 1)
+        #     # 随机选取一列用于where子句条件
+        #     where_col_type = 'float'
+        #     while where_col_type == 'float':
+        #         where_col_no = random.randint(0, len(mock[0]) - 1)
+        #         where_col_type = column_types[where_col_no]
+        #     where_rhs = mock[row_no][where_col_no]
+        #     where_clause = f"{column_names[where_col_no]} = {where_rhs}"
+        #     open(f"{cls.DB}/output.txt", "w").close()  # 清空输出，避免上轮输出的结果影响这轮测试
+        #     cls.client = subprocess.Popen([cls.CLIENT], stdin=subprocess.PIPE, preexec_fn=os.setsid)
+        #     cls.client.stdin.write(f"select * from {cls.TABLENAME} where {where_clause};\n".encode())
+        #     cls.client.stdin.close()    # client打开关闭放到循环体外面无法通过测试，原因未知
+        #     time.sleep(0.5)
+        #     with open(f"{cls.DB}/output.txt", "rt") as f:
+        #         lines = f.readlines()
+        #     result_list = []
+        #     # 遍历条件查询活动的结果，检查是否存在至少一行记录和之前随机选取的一行完全相同
+        #     for line in lines[1:]:
+        #         line = line.strip('|\n').split('|')
+        #         result = []
+        #         for col_no, col in enumerate(column_types):
+        #             lhs, rhs = line[col_no].strip(), mock[row_no][col_no]
+        #             if col == 'int':
+        #                 result.append(lhs == rhs)
+        #             elif col == 'float':
+        #                 result.append(float(lhs) - float(rhs) < 1e-4)
+        #             elif col.startswith('char('):
+        #                 result.append(lhs == rhs.strip("'"))
+        #             else:
+        #                 assert False, '不支持的类型'
+        #         result_list.append(all(result))
+        #     assert any(result_list)
+
 
     @classmethod
     def random_update(cls, column_types, column_names, mock):
@@ -82,6 +122,22 @@ class TestConditionalQuery:
         cls.client.stdin.write(sql.encode())
 
     @classmethod
+    def random_delete(cls, column_types, column_names, mock: list[list[str]]):
+        # 随机选取一行进行删除
+        row_no = random.randint(0, len(mock) - 1)
+        # 随机选取一列作为where条件，避免浮点数直接比较
+        # 生成表时已经保证了不会出现所有列都是浮点类型的情况
+        where_col_type = 'float'
+        while where_col_type == 'float':
+            where_col_no = random.randint(0, len(mock[0]) - 1)
+            where_col_type = column_types[where_col_no]
+        where_rhs = mock[row_no][where_col_no]
+        where_clause = f"{column_names[where_col_no]} = {where_rhs}"
+        sql = f"delete from {cls.TABLENAME} where {where_clause};\n"
+        cls.client.stdin.write(sql.encode())
+        mock.pop(row_no)
+
+    @classmethod
     def create_and_insert(cls):
         count = random.randint(1, 10)
         # 生成随机类型，不要全是浮点类型的
@@ -96,7 +152,7 @@ class TestConditionalQuery:
         # 组装sql，通过client发送
         cls.client.stdin.write(sql.encode())
         mock = []
-        for i in range(random.randint(1, 100)):
+        for i in range(random.randint(80, 200)):
             row = list(cls.generate_random_data(column_types))
             mock.append(row)
             sql = f"insert into {cls.TABLENAME} values({",".join(row)});\n"
